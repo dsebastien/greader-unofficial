0c4948ba583afe1d6e7e7f3e456c5a783cfca5e0
lechtitseb <lechtitseb@c701ff46-d34e-0410-9a32-37465e4991ce>
2008-07-30 16:56:42 +0000

    Initial upload

---
+++ src/main/java/be/lechtitseb/google/reader/api/model/authentication/GoogleCredentials.java
@@ -0,0 +1,58 @@
+package be.lechtitseb.google.reader.api.model.authentication;
+
+
+/**
+ * Google specific credentials
+ */
+public class GoogleCredentials extends BasicCredentials {
+	private String sid = "";
+	private String lSid = ""; // Unused for now it seems
+
+	public GoogleCredentials(String username, String password) {
+		super(username, password);
+	}
+
+	/**
+	 * Clear authentication information (sid, lsid)
+	 */
+	public void clearAuthentication() {
+		setSid(null);
+		setLSid(null);
+	}
+	
+	@Override
+	public void clearCredentials() {
+		super.clearCredentials();
+	}
+
+	public String getLSid() {
+		return lSid;
+	}
+
+	public String getSid() {
+		return sid;
+	}
+
+	/**
+	 * Whether we have SID + LSID or not
+	 */
+	public boolean hasAuthentication() {
+		return (!sid.equals("") && !lSid.equals(""));
+	}
+
+	public void setLSid(String lSid) {
+		if (lSid == null) {
+			this.lSid = "";
+		} else {
+			this.lSid = lSid;
+		}
+	}
+
+	public void setSid(String sid) {
+		if (sid == null) {
+			this.sid = "";
+		} else {
+			this.sid = sid;
+		}
+	}
+}
